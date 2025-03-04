5ed7ca78df7c88deffe63255ace063dfb26a2b3e
PiotrK202 <piotr.koscielny69@gmail.com>
2025-03-04 13:53:37 +0100

    Initial Commit

---
+++ ListOfItems/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListOfItems
+//
+//  Created by piotr koscielny on 04/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListOfItems/ListOfItemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfItemsApp.swift
+//  ListOfItems
+//
+//  Created by piotr koscielny on 04/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ListOfItemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
